a3d12817a9198117249554000a194bf214523745
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-04-02 03:18:42 +0800

    Add logging

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -6,9 +6,12 @@ import (
 	"github.com/alchermd/cyoa"
 	"net/http"
 	"os"
+	"log"
 )
 
 func main() {
+	log.Printf("Starting application")
+	
 	fileName := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	port := flag.Int("port", 8000, "the PORT to run the web server on")
 	flag.Parse()
@@ -24,5 +27,7 @@ func main() {
 	}
 
 	http.HandleFunc("/", cyoa.NewHandler(story))
+
+	log.Printf("Starting server on port: %d\n", *port)
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
